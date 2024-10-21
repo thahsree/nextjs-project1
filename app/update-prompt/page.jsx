36427bb80a87ac55d62eb3b299edb7506b58d9fa
thashreef <chthashreef22@gmail.com>
2024-10-21 15:19:18 +0400

    vercel commit

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,79 +1,82 @@
 'use client'
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from "react";
-
-
 import Form from '@components/Form';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense, useEffect, useState } from 'react';
 
 function UpdatePrompt(props) {
-
     const router = useRouter();
     const searchParams = useSearchParams();
     const promptId = searchParams.get('id');
 
-    const [submitting , setSubmitting ] = useState(false);
-    const [post , setPost] = useState({
-        prompt:'',
-        tag:''
-    })
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: ''
+    });
 
-
-    useEffect(()=>{
-        const getPromptDetails = async()=>{
+    useEffect(() => {
+        const getPromptDetails = async () => {
             const response = await fetch(`/api/prompt/${promptId}`);
-            
 
-            const data = await response.json()
-            console.log(data);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
 
-            setPost({
-                prompt: data.prompt,
-                tag:data.tag
-            })
-        
+                setPost({
+                    prompt: data.prompt,
+                    tag: data.tag
+                });
+            } else {
+                console.error('Failed to fetch prompt details');
+            }
+        };
+
+        if (promptId) getPromptDetails();
+    }, [promptId]);
+
+    const updatePrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+
+        if (!promptId) {
+            alert('Prompt ID not found');
+            return;
         }
 
-        if(promptId) getPromptDetails()
-    },[promptId])
-
-    
-    const updatePrompt = async(e)=>{
-         e.preventDefault();
-         setSubmitting(true);
-
-         if(!promptId) alert('Prompt ID not found');
-
-         try{
-
-            const response = await fetch(`/api/prompt/${promptId}`,{
-                method:'PATCH',
-                body:JSON.stringify({
+        try {
+            const response = await fetch(`/api/prompt/${promptId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
                     prompt: post.prompt,
                     tag: post?.tag
                 })
             });
 
-            if(response.ok){
-                router.push('/')
+            if (response.ok) {
+                router.push('/');
             }
 
-         }catch(error){
+        } catch (error) {
             console.log(error);
-         }finally{
+        } finally {
             setSubmitting(false);
-         }
-    }
+        }
+    };
+
     return (
-
-           <Form 
-            type='Edit'
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={updatePrompt}
-
-           />
+        <Suspense fallback={<div>Loading...</div>}>
+            <Form
+                type='Edit'
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
     );
 }
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,61 +1,96 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+
 import PromptCard from "./PromptCard";
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
-function Feed(props) {
-    const [searchText , setSearchText] = useState("");
-    const [posts , setPosts] = useState([])
-    const handleSearchText =()=>{}
+const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
 
-    const PromptCardList = ({data , handleTagClick})=>{
-        return(
-            <div className='mt-16 prompt_layout'>
-                {
-                    data.map((post)=> (
-                        <PromptCard 
-                            key={post._id}
-                            post={post}
-                            handleTagClick={handleTagClick}
-                        />
-                    ))
-                }
-            </div>
-        )
-    }
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-    useEffect(()=>{
-        const fetchPost = async()=>{
-            const response = await fetch('/api/prompt')
-            const data = await response.json()
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
 
-            setPosts(data)
-        }
+    setAllPosts(data);
+  };
 
-        fetchPost();
-    },[])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-
-    return (
-        <section className='feed'>
-            <form className='relative w-full flex-center'>
-                <input 
-                type="text" 
-                placeholder='search for a prompt' 
-                value={searchText}
-                onChange={handleSearchText}
-                required
-                className='search_input peer'
-                />
-            </form>
-            <PromptCardList
-             data={posts}
-             handleTagClick={()=>{}}
-
-            />
-        </section>
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
     );
-}
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
+    </section>
+  );
+};
 
 export default Feed;
